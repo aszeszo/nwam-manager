8ee74fb0f35c08c9ae593d33bc8b0be3c8c9f82f
dkenny <dkenny@6b7d1fb9-75dd-c4d7-8f26-8a8fef618664>
2009-09-07 10:56:35 +0000

    2009-09-07  Darren Kenny  <darren.kenny@sun.com>
    	* capplet/capplet-utils.c: 
    	  Fix defect#10875 where duplicate names are allowed to be given to
    	  locations. Adds a check before setting the name whether it
    	  already exists or not.
    
    
    
    git-svn-id: file:///export/mirror/svn/svn.opensolaris.org/svn/jds/nwam-manager/trunk@478 6b7d1fb9-75dd-c4d7-8f26-8a8fef618664

--- capplet/capplet-utils.c
+++ capplet/capplet-utils.c
@@ -74,6 +74,50 @@ capplet_list_foreach_merge_to_tree_store(gpointer data, gpointer user_data)
 	g_object_unref(data);
 }
 
+static gboolean
+capplet_model_foreach_find_by_name(GtkTreeModel *model,
+    GtkTreePath *path,
+    GtkTreeIter *iter,
+    gpointer user_data)
+{
+    CappletForeachData *data = (CappletForeachData *)user_data;
+    GObject        *object;
+    const gchar    *search_name = (const gchar*)data->user_data;
+    gboolean       *found_p = (gboolean*)data->ret_data;
+
+    gtk_tree_model_get( GTK_TREE_MODEL(model), iter, 0, &object, -1);
+
+    if ( object ) {
+        gchar   *name;
+
+        name = nwamui_object_get_name(NWAMUI_OBJECT(object));
+
+        if ( name != NULL && strcmp(name, search_name) == 0 ) {
+            *found_p = TRUE;
+        }
+    }
+
+    return( *found_p ); /* Stop search if we found something */
+}
+
+static gboolean 
+capplet_model_check_exists( GtkTreeModel *model, const gchar *name )
+{
+    CappletForeachData  data;
+    gboolean            exists = FALSE;
+    NwamuiDaemon       *daemon;
+
+    g_return_val_if_fail( model != NULL && name != NULL, exists );
+
+	data.user_data = (gpointer)name;
+	data.ret_data = (gpointer)&exists;
+
+	gtk_tree_model_foreach(model, capplet_model_foreach_find_by_name,
+                           (gpointer)&data);
+
+    return( exists );
+}
+
 /*
  * capplet_model_find_object:
  * @object: Could be null
@@ -317,7 +361,19 @@ nwamui_object_name_cell_edited ( GtkCellRendererText *cell,
 		NwamuiObject*  object;
 		gtk_tree_model_get(model, &iter, 0, &object, -1);
 
-		nwamui_object_set_name(object, new_text);
+        if ( !capplet_model_check_exists( model, new_text ) ) {
+            nwamui_object_set_name(object, new_text);
+        }
+        else {
+            GtkWindow  *top_level = GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(treeview)));
+            gchar      *message;
+
+            message = g_strdup_printf(_("The name '%s' is already in use"), new_text );
+
+            nwamui_util_show_message( top_level, GTK_MESSAGE_ERROR, _("Name Already In Use"), message, FALSE);
+
+            g_free(message);
+        }
 
 		g_object_unref(object);
 	}
